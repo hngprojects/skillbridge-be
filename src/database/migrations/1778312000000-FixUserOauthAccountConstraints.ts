87c547fb91046dbbdf25c2dedcdb2064849ec023
Lisan al Gaib <chidiobinna0001@gmail.com>
2026-05-09 21:47:43 +0100

    chore: refactor code for improved readability and consistency

--- src/database/migrations/1778312000000-FixUserOauthAccountConstraints.ts
+++ src/database/migrations/1778312000000-FixUserOauthAccountConstraints.ts
@@ -1,8 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class FixUserOauthAccountConstraints1778312000000
-  implements MigrationInterface
-{
+export class FixUserOauthAccountConstraints1778312000000 implements MigrationInterface {
   name = 'FixUserOauthAccountConstraints1778312000000';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
