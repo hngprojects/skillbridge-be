ac0904ad518b0be789d1821a9f47ea7b6f34162f
Deborah Anyachukwu <108271834+Zubbee18@users.noreply.github.com>
2026-05-09 06:19:55 +0100

    feat: create user_oauth_accounts migration and entity for OAuth integration

---
+++ src/modules/users/entities/user-oauth-account.entity.ts
@@ -0,0 +1,41 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from './user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  CANDIDATE = 'candidate',
+  EMPLOYER = 'employer',
+}
+
+@Entity('user_oauth_accounts')
+export class OAuthUser {
+  @ApiProperty({ format: 'uuid' })
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ApiProperty({ format: 'uuid' })
+  @Column()
+  user_id: string;
+
+  @OneToOne(() => User, (user: User) => user.id)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column({ type: 'varchar', length: 20, unique: true })
+  provider: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  provider_id: string;
+
+  @ApiProperty()
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
+  createdAt: Date;
+}
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModelAction } from './actions/user.action';
 import { User } from './entities/user.entity';
+import { OAuthUser } from './entities/user-oauth-account.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, OAuthUser])],
   controllers: [UsersController],
   providers: [UserModelAction, UsersService],
   exports: [UsersService, UserModelAction],
